6b708e0a488fe2c6596020e0011b92a3bb00813c
BRL4528 <blgc.sgo@hotmail.com>
2020-06-17 16:39:37 -0400

    ajuste server

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://api-twobrothers.herokuapp.com/',
-  baseURL: 'https://localhost:3000',
+  baseURL: 'https://api-twobrothers.herokuapp.com/',
+  // baseURL: 'https://localhost:3000',
 });
 
 export default api;
