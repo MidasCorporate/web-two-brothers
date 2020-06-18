ffdc680e42657fbac7606d55e000761209df1aa2
Bruno Luiz <50926585+BRL4528@users.noreply.github.com>
2020-06-18 08:45:41 -0400

    test router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+//ReactDOM.render(
  // <React.StrictMode>
-   <BrowserRouter>
-    <App />
-  </ BrowserRouter>,
+  // <BrowserRouter>
+   // <App />
+  //</ BrowserRouter>,
  // </React.StrictMode>,
-  document.getElementById('root')
-);
+//  document.getElementById('root')
+//);
+ReactDOM.render((
+<BrowserRouter>
+  <App/>
+</BrowserRouter>),
+document.getElementById('root'))
