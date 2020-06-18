ab475211c93c10622ae6ae232742f663cc5c8e9c
Bruno Luiz <50926585+BRL4528@users.noreply.github.com>
2020-06-18 09:26:26 -0400

    update BrowserRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
+import { BrowserRouter } from 'react-router-dom';
 
 import SingnIn from '../pages/SignIn';
 
@@ -9,11 +10,13 @@ import DashboardAdm from '../pages/admin/DasboardAdm';
 
 export default function Routes() {
   return (
+    <BrowserRouter>
     <Switch>
       <Route exact path="/" component={DashboardClient} />
       <Route exact path="/adm" component={SingnIn} />
 
       <Route exact path="/dashboardadm" component={DashboardAdm} isPrivate />
     </Switch>
+    </ BrowserRouter>
   );
 }
