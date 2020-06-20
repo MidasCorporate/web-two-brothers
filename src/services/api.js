1ea3b6ec9084f3531718ed15088b1c352fddaa52
BRL4528 <blgc.sgo@hotmail.com>
2020-06-20 11:58:56 -0400

    update window banner url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api-twobrothers.herokuapp.com/',
+  baseURL: 'https://api-twobrothers.herokuapp.com',
   // baseURL: 'http://localhost:3333',
 });
 
