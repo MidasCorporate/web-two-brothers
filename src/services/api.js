85a200266dd5afbff42c2a3d98f94c351db680ef
BRL4528 <blgc.sgo@hotmail.com>
2020-06-21 12:40:11 -0400

    update url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api-twobrothers.herokuapp.com',
+  // baseURL: 'https://api-twobrothers.herokuapp.com',
   // baseURL: 'http://localhost:3333',
+  baseURL: 'https://157.245.80.142',
 });
 
 export default api;
