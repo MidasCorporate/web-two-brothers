cb58b93cfc0056c8e11532eb7c66318e782c6ccd
alessandro-silva <alessandro.rds03@gmail.com>
2020-06-24 13:07:22 -0400

    :leaves: add responsiveness in page admin

--- src/pages/admin/DasboardAdm/index.js
+++ src/pages/admin/DasboardAdm/index.js
@@ -1,15 +1,9 @@
-/**
- * OS COMPONENTE ABOUT E EVALUENTION ESTA DISPONIVEL PARA FUTUROS UPGRADES
- */
-
 import React from 'react';
 
 import { Container, Grid } from './styles/styles';
 
 import Header from '~/components/HeaderAdm';
 import BannerFunction from './Banner';
-// import AboutFunction from './About';
-// import EvaluentionFunction from './Evaluention';
 import MessageFunction from './Message';
 
 export default function Dashboard() {
@@ -19,8 +13,6 @@ export default function Dashboard() {
       <Container>
         <Grid>
           <BannerFunction />
-          {/* <AboutFunction /> */}
-          {/* <EvaluentionFunction /> */}
           <MessageFunction />
         </Grid>
       </Container>
--- src/pages/admin/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-function admin() {
-  return <div />;
-}
-
-export default admin;
